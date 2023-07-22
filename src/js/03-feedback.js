799fe6de3c8652c37156e0f380253110da881631
romanfliak <rfliak568@gmail.com>
2023-07-22 16:44:04 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,37 @@
 import throttle from "lodash.throttle";
 
-document.addEventListener("DOMContentLoaded", function () {
+
     const form = document.querySelector(".feedback-form");
     const emailInput = form.querySelector("input[name='email']");
     const messageInput = form.querySelector("textarea[name='message']");
-  
-    const saveFormData = throttle(() => {
+    const storageKey = 'feedback-form-state';
+    
+    
+    const savedFormData = throttle(() => {
       const formData = {
         email: emailInput.value,
         message: messageInput.value,
       };
       localStorage.setItem("storageKey", JSON.stringify(formData));
     }, 500);
-  
-    const loadFormData = () => {
-      const savedFormData = JSON.parse(localStorage.getItem("feedback-form-state"));
+  form.addEventListener('input', savedFormData)
+    
+  const loadFormData = () => {
+      const savedFormData = JSON.parse(localStorage.getItem(storageKey));
       if (savedFormData) {
-        emailInput.value = savedFormData.email;
-        messageInput.value = savedFormData.message;
+        emailInput.value = savedFormData.email || '';
+        messageInput.value = savedFormData.message || '';
       }
     };
-  
-    const clearFormData = () => {
-      localStorage.removeItem("feedback-form-state");
-    
-      console.log("Form submitted with data:", {
-        email: emailInput.value,
-        message: messageInput.value,
-      });
-    };
-    emailInput.addEventListener("input", saveFormData);
-    messageInput.addEventListener("input", saveFormData);
-    form.addEventListener("submit", function (event) {
-      event.preventDefault();
-      clearFormData();
-    });
-  
+  form.addEventListener('submit', event =>{
+    event.preventDefault();
+    console.log({
+    email: messageInput.value,
+    message : messageInput.value,
+    })
+   localStorage.removeItem(storageKey);
+   emailInput.value = '';
+   messageInput.value = ''
+  })
     loadFormData();
-  });
   
